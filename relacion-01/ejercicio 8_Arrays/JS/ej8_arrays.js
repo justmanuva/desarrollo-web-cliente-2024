e9db9aaadc2c23918a3af56fa8c8f915c65815fb
Manuva <minimanuva@gmail.com>
2024-10-09 18:38:59 +0200

    ej8 updated

--- relacion-01/ejercicio 8_Arrays/JS/ej8_arrays.js
+++ relacion-01/ejercicio 8_Arrays/JS/ej8_arrays.js
@@ -1,3 +1,34 @@
-function escribirArrayUnico(array1, array2){
-  concatenado = [];
+function recogeNombres(){
+  array = prompt("Inserta diferentes nombres separados por ','");
+  arraySeparado = array.split(",");
+  return arraySeparado;
 }
+
+array1 = recogeNombres();
+array2 = recogeNombres();
+
+function quitarIguales(array1, array2) {
+  let arrayCorregido = [];
+
+  let maxLength = array2.length;
+  if (array1.length > array2.length) maxLength = array1.length;
+
+  for (let i = 0; i < array1.length; i++) {
+    let encontrado = false;
+    for (let j = 0; j < array2.length; j++) {
+      if (array1[i] == array2[j]) encontrado = true;
+    }
+    if (!encontrado) arrayCorregido.push(array1[i]);
+  }
+
+  for (let i = 0; i < array2.length; i++) {
+    let encontrado = false;
+    for (let j = 0; j < array1.length; j++) {
+      if (array2[i] == array1[j]) encontrado = true;
+    }
+    if (!encontrado) arrayCorregido.push(array2[i]);
+  }
+  return arrayCorregido;
+}
+
+console.log(quitarIguales(array1, array2));

8a1eaee76983b6018dc6c507bc857f11cfff43ea
justmanuva <manuva2662004@gmail.com>
2024-10-14 12:07:40 +0200

    Ejercicio 15 y 16 a medio hacer y 8 corregido

--- relacion-01/ejercicio 8_Arrays/JS/ej8_arrays.js
+++ relacion-01/ejercicio 8_Arrays/JS/ej8_arrays.js
@@ -7,6 +7,7 @@ function recogeNombres(){
 array1 = recogeNombres();
 array2 = recogeNombres();
 
+//Mi función (falta comprobar en el mismo array)
 function quitarIguales(array1, array2) {
   let arrayCorregido = [];
 
@@ -28,4 +29,49 @@ function quitarIguales(array1, array2) {
   return arrayCorregido;
 }
 
-console.log(quitarIguales(array1, array2));
+//Función del profe
+function copiaUnicos(array1, array2) {
+  let arrayFinal = [];
+
+  //Agregar elementos únicos de array1 que no estén repetidos en array1 ni esten en array2
+  for (let i = 0; i < array1.length; i++) {
+    if (!array2.includes(array1[i]) && esUnico(array1, array1[i])) {
+      arrayFinal.push(array1[i]);
+    }
+    // if (array2.indexOf(array1[i]) === -1 && esUnico(array1, array1[i])) {
+    //   arrayFinal.push(array1[i]);
+    // }
+  }
+  //Agregar elementos únicos de array2 que no estén repetidos en array2 ni esten en array1
+  for (let i = 0; i < array2.length; i++) {
+    if (!array1.includes(array2[i]) && esUnico(array2, array2[i])) {
+      arrayFinal.push(array2[i]);
+    }
+    // if (array1.indexOf(array2[i]) === -1 && esUnico(array2, array2[i])) {
+    //   arrayFinal.push(array2[i]);
+    // }
+  }
+  return arrayFinal;
+}
+
+//Igual que repetidos pero más simple
+function esUnico(array, elem) {
+  return array.indexOf(elem) === array.lastIndexOf(elem);
+}
+
+function repetidos(array, elem) {
+  let cont = 0;
+
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === elem) cont++;
+  }
+
+  // if (cont == 1) return false;
+  // return true;
+
+  //Mi simplificación del return
+  return cont != 1;
+}
+
+console.log(copiaUnicos(array1, array2));
+// console.log(quitarIguales(array1, array2));
